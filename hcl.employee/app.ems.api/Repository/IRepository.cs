e29e37f6097e5480e8d979601a7b736fa6260311
Darek <dariusz.pelka@aerdata.com>
2021-08-08 10:22:06 +0000

    Add respository class

---
+++ hcl.employee/app.ems.api/Repository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using app.ems.api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.ems.api.Repository
+{
+    interface IRepository
+    {
+        IQueryable<Employee> GetEmployees();
+
+        void AddEmployee(Employee model);
+
+        void AmendEmployee(Employee model);
+
+        void RemoveEmployee(int id);
+
+        bool EmployeeExists(int id);
+    }
+}
---
+++ hcl.employee/app.ems.api/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using app.ems.api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace app.ems.api.Repository
+{
+    public class Repository : IRepository
+    {
+        private EmployeeManagementSystemEntities db = new EmployeeManagementSystemEntities();
+
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        public void AddEmployee(Employee model)
+        {
+            db.Employees.Add(model);
+            db.SaveChangesAsync();
+        }
+
+        public void AmendEmployee(Employee model)
+        {
+            db.Entry(model).State = EntityState.Modified;
+            db.SaveChangesAsync();
+        }
+
+        public void RemoveEmployee(int id)
+        {
+            var employee = db.Employees.FirstOrDefault(m => m.Id == id);
+            db.Employees.Remove(employee);
+            db.SaveChangesAsync();
+        }
+
+        public bool EmployeeExists(int id)
+        {
+            var employee = db.Employees.FirstOrDefault(m => m.Id == id);
+            return employee == null;
+        }
+    }
+}
